c152175d9155f7cb4dc6e1a6cf7a0bb825c12c89
JakobKvejborg <you@example.com>
2024-01-02 12:24:31 +0100

    Adds buttons!

--- javascript.js
+++ javascript.js
@@ -1,15 +1,25 @@
-// Create a function to handle button click
+// Button "Click here"
+const btn1 = document.getElementById("btn1");
+btn1.addEventListener("click", function(e) {
+    const name = prompt("What is your name?");
+    console.log(name); // this prints the name entered in the console
+    if (name != null) {
+        document.body.innerHTML = "<h1>Welcome " + name + "!</h1>";
+    }
+});
+
+// Button "Open Google in new tab"
 function openGoogle() {
     window.open('https://www.google.com', '_blank');
 }
+const btn4 = document.getElementById("btn4");
+btn4.addEventListener("click", openGoogle);
+document.body.appendChild(btn4);
 
-// Create the button element
-const myButton = document.createElement('button');
-myButton.textContent = 'Go to Google'; // Set the button text
-myButton.classList.add('button'); // Add the 'button' class for styling
-
-// Add click event listener to the button
-myButton.addEventListener('click', openGoogle);
-
-// Append the button to the body of the document
-document.body.appendChild(myButton);
+// Button "Youtube"
+function openYoutube() {
+    window.open('https://www.youtube.com', '_blank');
+}
+const btn5 = document.getElementById("btn5");
+btn5.addEventListener("click", openYoutube);
+document.body.appendChild(btn5);
